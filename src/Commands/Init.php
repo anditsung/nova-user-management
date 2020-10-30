9bf53bf0e723fa5c5b7a9710121bc34e79064db7
anditsung <anditsung@gmail.com>
2020-10-30 14:13:22 +0700

    penambahan permission viewLog yang digunakan oleh https://github.com/KABBOUCHI/nova-logs-tool

--- src/Commands/Init.php
+++ src/Commands/Init.php
@@ -117,6 +117,7 @@ class Init extends Command
             'viewNova' => 'System',
             'viewTelescope' => 'System',
             'viewBackup' => 'System',
+            'viewLog' => 'System',
             'api' => 'System',
             'viewAny action-resources' => 'Actions',
             'view action-resources' => 'Actions',
